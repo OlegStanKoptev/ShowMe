42543d68374d797f9f263c06ad32c1f3e11803c1
Oleg Koptev <olegstankoptev@gmail.com>
2020-10-15 22:47:47 +0300

    Finishing touches

---
+++ ShowMe/MessageViewController.swift
@@ -0,0 +1,32 @@
+//
+//  MessageViewController.swift
+//  ShowMe
+//
+//  Created by Oleg Koptev on 15.10.2020.
+//
+
+import UIKit
+
+class MessageViewController: UIViewController {
+    @IBOutlet weak var messageLabel: UILabel!
+    var messageData: String?
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        messageLabel.text = messageData
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
--- ShowMe/ViewController.swift
+++ ShowMe/ViewController.swift
@@ -8,12 +8,23 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var textToSendField: UITextField!
+    @IBAction func showMe(_ sender: Any) {
+        NSLog("User Wrote: %@", textToSendField.text!)
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
-
-
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        let messageController = segue.destination as! MessageViewController
+        messageController.messageData = textToSendField.text
+    }
 }
 
