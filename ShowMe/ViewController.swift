9143fe636da0df6f99c618911431438dc1c91a0e
Oleg Koptev <olegstankoptev@gmail.com>
2020-10-15 21:52:08 +0300

    Initial Commit

---
+++ ShowMe/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ShowMe
+//
+//  Created by Oleg Koptev on 15.10.2020.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
